4c597990c86d95be4eed6d8a4e5a0a608ae03e9d
Adam Frewin <AdamFrewin@Adams-MacBook-Pro.local>
2018-12-28 12:46:55 -0500

    Deleting stuff

--- lift_curve.m
+++
@@ -1,9 +0,0 @@
-function a = lift_curve(AR, Mach, sweep)
-if AR<4
-    k = 1 + AR*(1.87 - 2.33e-4*sweep)/100;
-else
-    k = 1 + (8.2 - 2.3*sweep - AR*(.22-.153*sweep))/100;
-end
-
-a = 2*pi*AR./(2+sqrt(AR^2.*(1-Mach.^2)./k^2 .* (1 + tan(sweep)^2./(1 - Mach.^2))+4));
-end
