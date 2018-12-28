dace910d6fce7896318f784ed39db5d1bd2f10c7
Adam Frewin <AdamFrewin@Adams-MacBook-Pro.local>
2018-12-28 12:51:24 -0500

    Deleting more

--- downwash_slope.m
+++
@@ -1,4 +0,0 @@
-function de_dalpha = downwash_slope(AR, Mach, ltv, taper, span, sweep)
-de_dalpha = 4.44*sqrt(1-Mach.^2).*((1/AR - 1/(1 + AR^1.7))*(10-3*taper)/7.*...
-    (1 - ltv/span)/((2*lt/b)^.33)*sqrt(cos(sweep))).^1.19;
-end
